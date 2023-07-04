02be4dd757075bbc40fe2924113c0bba8061ce0e
Crow Rising <miscbyrd@gmail.com>
2023-07-04 13:50:37 -0600

    fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,11 +13,4 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password) }
     it { should have_secure_password }
   end
-
-    # it 'can validate secure password' do
-    #   user = User.create(name: 'Bob', email: 'gaga@aol.com', password: '123', password_confirmation: '123')
-
-    #   # expect(user).to_not have_attributes(password: '123')
-    #   expect(user.password_digest).to_not eq('123')
-    # end
 end
