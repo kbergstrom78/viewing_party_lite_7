b924c1e51e75fa8e74224c72a974f3e8e189c114
Crow Rising <miscbyrd@gmail.com>
2023-07-05 15:40:02 -0600

    test: add test for discover movie page

---
+++ spec/features/users/discover/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Discover Movies Page' do  
+  describe 'as a user when I visit the discover movies page' do 
+    before :each do
+      @user1 = User.create!(name: 'Danny', email: 'callmeDan@yaho.com')
+      @user2 = User.create!(name: 'Sandy', email: 'itsSandyifyounasty@hotmail.com')
+    end
+
+    it 'I see a button to discover top rated movies', :vcr do
+      visit user_discover_index_path(@user1.id)
+
+      expect(page).to have_content('Discover Movies Page')
+      within '#top-rated' do
+        expect(page).to have_button('Discover Top Rated Movies')
+
+        click_button 'Discover Top Rated Movies'
+      end
+      expect(current_path).to eq(user_movie_index_path(@user1.id))
+      expect(page).to have_content('Movies Results Page')
+    end
+
+    end
+  end
