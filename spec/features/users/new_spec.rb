75a565e1de81ffe8fb56957bb0590768e83b1eec
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-05 17:54:06 -0600

    test: add test for sad path testing users#create

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -26,5 +26,28 @@ RSpec.describe 'User Registration' do
 
       expect(current_path).to eq(dashboard_path)
     end
+
+    it 'will not create a user with incomplete information' do
+      visit '/register'
+
+      within '#registration-form' do
+        fill_in 'Name', with: 'Bob'
+        fill_in 'Email', with: 'gohomenow@hotmail.com'
+
+        click_button 'Save'
+      end
+      expect(current_path).to eq(dashboard_path)
+
+      visit '/register'
+
+      within '#registration-form' do
+        fill_in 'Name', with: 'Bob'
+        fill_in 'Email', with: 'gohomenow@hotmail.com'
+
+        click_button 'Save'
+      end
+      save_and_open_page
+      expect(page).to have_content('Try again! All fields must be complete and email unique.')
+    end
   end
 end
