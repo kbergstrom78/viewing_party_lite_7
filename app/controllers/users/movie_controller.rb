d55c20b9865ce81e2cd04434ba5f093d6746c99b
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-06 16:14:53 -0600

    fix: add cast and reviews to #show

--- app/controllers/users/movie_controller.rb
+++ app/controllers/users/movie_controller.rb
@@ -14,6 +14,8 @@ module Users
     def show
       @user = User.find(params[:user_id])
       @movie = MovieFacade.get_movie(params[:id])
+      @cast = MovieFacade.top_ten_cast(params[:id])
+      @reviews = MovieFacade.reviews(params[:id])
     end
   end
 end
