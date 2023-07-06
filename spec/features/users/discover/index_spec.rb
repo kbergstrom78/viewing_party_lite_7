1dfb15a135d8377027511ccb7a90eb8cc22036a2
Crow Rising <miscbyrd@gmail.com>
2023-07-05 18:16:56 -0600

    feat: add top_rated in the index action

--- spec/features/users/discover/index_spec.rb
+++ spec/features/users/discover/index_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe 'Discover Movies Page' do  
-  describe 'as a user when I visit the discover movies page' do 
+RSpec.describe 'Discover Movies Page' do
+  describe 'as a user when I visit the discover movies page' do
     before :each do
       @user1 = User.create!(name: 'Danny', email: 'callmeDan@yaho.com')
       @user2 = User.create!(name: 'Sandy', email: 'itsSandyifyounasty@hotmail.com')
@@ -19,6 +21,5 @@ RSpec.describe 'Discover Movies Page' do
       expect(current_path).to eq(user_movie_index_path(@user1.id))
       expect(page).to have_content('Movies Results Page')
     end
-
-    end
   end
+end
