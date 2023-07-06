92c9bef66a4c8d8fce2c67c9620ac40c46aa8966
Crow Rising <miscbyrd@gmail.com>
2023-07-06 17:28:17 -0600

    test: add test for viewing party page

---
+++ spec/features/users/movies/viewing_parties/new_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Viewing Party New Page' do
+  describe 'as a user when I visit the new viewing party page' do
+    before :each do
+      @user1 = User.create!(name: 'Danny', email: 'hahaHA@yahoo.com')
+      @movie = Movie.new(title: 'The Godfather', id: 238, runtime: 175)
+      visit new_user_movie_viewing_party_path(@user1, @movie.id)
+    end
+
+    it 'displays the movie title, and a form for party', :vcr do
+      expect(page).to have_content('The Godfather')
+
+      within '#party-details' do
+        expect(page).to have_content('Duration of Party')
+        expect(page).to have_content('Date')
+        expect(page).to have_content('Start Time')
+        expect(page).to have_field('checkbox_id', type: 'checkbox')
+        expect(page).to have_button('Create Party')
+      end
+    end
+  end
+end
