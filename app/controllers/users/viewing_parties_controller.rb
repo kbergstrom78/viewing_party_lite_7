e544d684a0bed939fc77aaee97412301c74b269e
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-06 19:06:12 -0600

    chore: add #create for viewingparties

--- app/controllers/users/viewing_parties_controller.rb
+++ app/controllers/users/viewing_parties_controller.rb
@@ -3,6 +3,22 @@ module Users
     def new
       @user = User.find(params[:user_id])
       @movie = MovieFacade.get_movie(params[:movie_id])
+      @users = User.all
+    end
+
+    def create
+      @new_party = ViewingParty.new(party_params)
+      if @new_party.save
+        redirect_to dashboard_path(current_user)
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def party_params
+      params.permit(:movie_id, :duration, :party_date, :party_time, user_ids: [])
     end
   end
 end
