47227e84534d351e608b6603704a3c3d0aee8605
Crow Rising <miscbyrd@gmail.com>
2023-07-07 11:42:21 -0600

    chore: add create action to viewing parties controller

--- app/controllers/users/viewing_parties_controller.rb
+++ app/controllers/users/viewing_parties_controller.rb
@@ -2,10 +2,14 @@
 
 module Users
   class ViewingPartiesController < ApplicationController
+
     def new
       @user = User.find(params[:user_id])
       @movie = MovieFacade.get_movie(params[:movie_id])
       @users = User.all
+    end
+
+    def create;
     end
   end
 end
