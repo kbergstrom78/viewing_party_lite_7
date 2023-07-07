143a0f6073eedbddbbb00a76814d03b428c00fdf
Crow Rising <miscbyrd@gmail.com>
2023-07-07 11:43:37 -0600

    chore: add create route to users viewing parties dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
   get '/dashboard', to: 'users#show'
+  post '/dashboard', to: 'users/viewing_parties#create'
 
   resources :users do
     resources :discover, only: [:index], controller: 'users/discover'
